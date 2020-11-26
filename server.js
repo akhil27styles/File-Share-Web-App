0be38ae77beb011c25ee910094ec67c1b654f291
akhil27styles <52193850+akhil27styles@users.noreply.github.com>
2020-11-26 12:44:39 +0530

    update server

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
-const express=require('express');
-const app=express();
-const path=require('path');
-const cors=require('cors');
-const PORT=process.env.PORT||3000;
-app.use(express.static('public'));
-const connectDB=require('./config/db');
-connectDB();
-const corsOptions={
-    origin:process.env.ALLOWED_CLIENTS.split(',')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
-app.use(cors(corsOptions));
-//Template engine
+
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+
+app.use(cors(corsOptions))
+app.use(express.static('public'));
+
+const connectDB = require('./config/db');
+connectDB();
+
 app.use(express.json());
-app.set('views',path.join(__dirname,'/views'));
-app.set('view engine','ejs');
-//Routes
-app.use('/api/files',require('./routes/files'));
-app.use('/files',require('./routes/show'));
-app.use('/files/download',require('./routes/download'));
-app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`);
-})
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+// Routes 
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
