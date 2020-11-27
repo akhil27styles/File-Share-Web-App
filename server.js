f0ceec8fb0297e188c60a3c1ec98859f06dcfd2b
akhil27styles <52193850+akhil27styles@users.noreply.github.com>
2020-11-27 23:30:01 +0530

    port

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 // Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
+// const corsOptions = {
+//   origin: process.env.ALLOWED_CLIENTS.split(',')
+//   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+// }
 
 // Default configuration looks like
 // {
