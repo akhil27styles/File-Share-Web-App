14ad86034a5bff1dc80af722411dcdb40a7eb041
Akhil Kumar Singh <52193850+akhil27styles@users.noreply.github.com>
2020-12-07 21:30:21 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ||'3000';
 const path = require('path');
 const cors = require('cors');
 // Cors 
